fa0e1af05834c27523607c36587adcaa8eb97b5c
lalkovski <ilija.lalkovski@inplayer.com>
2020-01-09 13:43:14 +0100

    Updates

---
+++ precache-manifest.42e4f0f0570f682833574f934044a398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776bf40d9572504952c960186bf24f75",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5341d0d6ef796f0d667c",
+    "url": "/monsters-rolodex/static/css/main.41028b17.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5341d0d6ef796f0d667c",
+    "url": "/monsters-rolodex/static/js/main.d979a8cb.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
